044acd05dc79d86994784158f7bca42368518fd5
Sulhadin Öney <sulhadin.oney@optiim.com>
2021-01-24 00:29:31 +0300

    feat: InputText created.

---
+++ src/components/InputText.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface IInputText {
+  onEnter: (value: string) => void;
+  placeholder: string;
+}
+
+const InputText: React.FC<IInputText> = ({ onEnter, placeholder }) => {
+  const [inputValue, setValue] = React.useState('');
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (event.key === 'Enter') {
+      onEnter(inputValue);
+      setValue('');
+    }
+  };
+
+  return (
+    <div className="">
+      <input
+        value={inputValue}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyPress={onKeyDown}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default InputText;
