37f722e6d19ecf7da77a6f600bc09472e83f3705
sulhadin <sulhadin@gmail.com>
2021-01-28 22:11:13 +0300

    fix: Type error resolved.

--- src/controllers/SpeechToText.ts
+++ src/controllers/SpeechToText.ts
@@ -1,19 +1,27 @@
-const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+declare global {
+  interface Window {
+    webkitSpeechRecognition: unknown;
+    webkitSpeechGrammarList: unknown;
+  }
+}
 
-const recognition = new SpeechRecognition();
+const { webkitSpeechRecognition, webkitSpeechGrammarList }: Window = window as Window;
 
-function TextToSpeech(computerWord: string) {
+const SR = SpeechRecognition || webkitSpeechRecognition;
+const SG = SpeechGrammarList || webkitSpeechGrammarList;
+
+const recognition = new SR();
+
+function TextToSpeech(computerWord: string): void {
   const synth = window.speechSynthesis;
   const utterThis = new SpeechSynthesisUtterance(computerWord);
   synth.speak(utterThis);
 }
 
-function SpeechToText(lang: string, gr: [string] | [] = []) {
+function SpeechToText(lang: string, gr: [string] | [] = []): () => void {
   const grammar = `#JSGF V1.0; grammar words; public <color> = ${gr.join(' | ')} ;`;
-  console.log(grammar);
 
-  const speechRecognitionList = new SpeechGrammarList();
+  const speechRecognitionList = new SG();
   speechRecognitionList.addFromString(grammar, 1);
   recognition.grammars = speechRecognitionList;
   recognition.continuous = false;
@@ -21,14 +29,12 @@ function SpeechToText(lang: string, gr: [string] | [] = []) {
   recognition.interimResults = false;
   recognition.maxAlternatives = 1;
 
-  console.log('Ready to receive a color command.');
-
   return () => {
     recognition.start();
   };
 }
 
-recognition.onresult = function (event: SpeechRecognitionEvent) {
+recognition.onresult = (event: SpeechRecognitionEvent) => {
   document.dispatchEvent(
     new CustomEvent('speechEvent', {
       detail: { result: event.results[0][0].transcript },
@@ -40,12 +46,8 @@ recognition.onspeechend = () => {
   recognition.stop();
 };
 
-recognition.onnomatch = (event: SpeechRecognitionEvent) => {
-  console.log('onnomatch', event);
-};
-
-recognition.onerror = (event) => {
-  console.log('onerror', event.error);
+recognition.onerror = () => {
+  recognition.stop();
 };
 
 export { TextToSpeech, SpeechToText };
