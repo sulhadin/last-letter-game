24eb834716458c69abc9b150bffe8395c353589e
sulhadin <sulhadin@gmail.com>
2021-02-10 00:41:09 +0300

    doc(InputWord.ts): Added code documentation.

--- src/presenters/game/inputs/InputWord.tsx
+++ src/presenters/game/inputs/InputWord.tsx
@@ -1,19 +1,28 @@
-import React, { memo, useContext, useEffect, useMemo } from 'react';
+import React, { memo, useContext, useEffect } from 'react';
 
 import GameContext from '../../../context/GameContext';
 
-import { getPlayer } from '../../../controllers/playerController';
+import { getPlayerType } from '../../../controllers/playerController';
 import useAIPlayers from '../../../hooks/useAIPlayers';
 import { IInput } from '../../../utils/types';
 import Inputs from './Inputs';
 
+/**
+ * Users are managed in the InputWord, Every user takes it's turn as 'playerType' that means 'currentPlayer' changes.
+ *
+ * AI players and Human player have been taken into account; however, players can vary. Might need a minor refactor to implement multiplayer.
+ * @param {function} onNewWord - Callback function listening new word.
+ * @param {string} placeholder - A default placeholder.
+ * @constructor
+ * @return {React.FC<IInput>} Input component.
+ */
 const InputWord: React.FC<IInput> = ({ onNewWord, placeholder }) => {
   const { state } = useContext(GameContext);
   const [disabled, setDisabled] = React.useState<boolean>(false);
 
-  const playerType = useMemo(() => getPlayer(state.currentPlayer, state.players), [
-    state.currentPlayer,
-  ]);
+  // Gets current player type immediately to determine which player should be the next.
+  const playerType = getPlayerType(state.currentPlayer, state.players);
+
   const { response, play } = useAIPlayers(playerType, state.currentWord);
 
   useEffect(() => {
@@ -21,6 +30,10 @@ const InputWord: React.FC<IInput> = ({ onNewWord, placeholder }) => {
   }, [response]);
 
   useEffect(() => {
+    // Any user can later log into the app and get a unique id.
+    // currentUser is planned to represent that unique id; so, more then one user can sign in.
+    // for now, if currentUser is not equals to currentPlayer whom is to play next, it is considered ai.
+    // with a small refactor, human players can be implemented..
     const isAi = state.currentPlayer !== state.currentUser;
     setDisabled(isAi);
 
