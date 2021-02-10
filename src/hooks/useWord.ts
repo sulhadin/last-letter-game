f835cf747c26a3482c907891138a324503682ab5
sulhadin <sulhadin@gmail.com>
2021-02-10 17:05:42 +0300

    docs(useWord.ts): Adds code documentation
    * fix restricted mode check.

--- src/hooks/useWord.ts
+++ src/hooks/useWord.ts
@@ -13,6 +13,26 @@ import {
 import { IResult, TWordResult } from '../utils/types';
 import { TUseWord } from './types';
 
+/**
+ * A React hook that validates the spoken word and saves it and delivers a result {@link TWordResult} in return.
+ *
+ * There is a business rule for validating a word.
+ *  - A response should not be empty
+ *  - A new word should be found by players
+ *  - Last word should start with previous word's start|end letters in regarding with specified preferences.
+ *  - Last word should not be spoken before in restricted mode which can be changed in preferences.
+ *
+ *  If all conditions satisfied,
+ *    adds the new word [Add logic]{@link addNewWord}
+ *    dispatches the global state.
+ *    updates local response state {@link TWordResult} as valid
+ *  If not
+ *    updates local response state {@link TWordResult} as invalid with a fail message
+ *
+ * @func useWord
+ * @memberOf React
+ * @return {TUseWord}
+ */
 const useWord = (): TUseWord => {
   const { state, dispatch } = useContext(GameContext);
 
@@ -27,16 +47,18 @@ const useWord = (): TUseWord => {
       return invalidResult(response.response);
     }
 
-    const { charLength, letterFromEnd } = state.preferences;
+    const { charLength, letterFromEnd, restricted } = state.preferences;
 
     const isValid = checkWord(state.currentWord, response.response, charLength, letterFromEnd);
     if (!isValid) {
       return invalidResult(`"${response.response}" is invalid, game over!`);
     }
 
-    const isExist = isWordExist(response.response, state.game);
-    if (isExist) {
-      return invalidResult(`"${response.response}" exists, game over!`);
+    if (restricted) {
+      const isExist = isWordExist(response.response, state.game);
+      if (isExist) {
+        return invalidResult(`"${response.response}" exists, game over!`);
+      }
     }
 
     return validResult();
@@ -45,7 +67,7 @@ const useWord = (): TUseWord => {
   const saveWord = useCallback(
     (response: IResult) => {
       if (state.timer.timeIsUp) {
-        setWordResponse(invalidResult('Your answer is invalid.'));
+        setWordResponse(invalidResult('Your answer is invalid!'));
         return; // Block game.
       }
 
