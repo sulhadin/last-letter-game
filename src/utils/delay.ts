7f89bc0c00ff78c13e36faa19315750cc0730418
sulhadin <sulhadin@gmail.com>
2021-02-10 09:53:21 +0300

    docs(delay.ts): Added code documentation.

--- src/utils/delay.ts
+++ src/utils/delay.ts
@@ -3,8 +3,13 @@ import randomize from './randomize';
 /**
  * Prepare a setTimeout function with random or static ms.
  *
- * @param callback
- * @param ms Either can be range or number.
+ * If ms is array, that array should has start-end values. Random value will be calculated based on start-end values
+ * and use that random value as ms itself inside 'setTimeout'
+ *
+ * @param {function} callback - A callback function to be run in 'setTimeout'.
+ * @param {number|[number,number]} ms Either can be range or number.
+ * @return {void}
+ * @see [Randomize]{@link randomize}
  */
 function delay(callback: () => void, ms: number | [number, number]): void {
   let time;
