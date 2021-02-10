849e978e1f911731b17e2d90e6a8c9e58a07c16d
sulhadin <sulhadin@gmail.com>
2021-02-10 17:06:10 +0300

    docs(GameContext.ts): Adds code documentation.

--- src/context/GameContext.ts
+++ src/context/GameContext.ts
@@ -2,6 +2,13 @@ import { createContext, Dispatch } from 'react';
 import { TInitialState } from '../store/types';
 import { initialState } from '../store/reducer';
 
+/**
+ * @typedef {state: TInitialState; dispatch: Dispatch<any>; }
+ * @property {TInitialState} state
+ * @property {Dispatch<any>} dispatch
+ */
+
+/** @type {import('react').Context<{state: TInitialState; dispatch: Dispatch<any>; }>} */
 const GameContext = createContext<{
   state: TInitialState;
   dispatch: Dispatch<any>;
