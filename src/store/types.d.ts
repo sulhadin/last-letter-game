4887ed3e76608a6522cad743ab7549adb75ddebe
sulhadin <sulhadin@gmail.com>
2021-02-10 09:55:50 +0300

    docs(types.d.ts): Added code documentation.

--- src/store/types.d.ts
+++ src/store/types.d.ts
@@ -1,5 +1,6 @@
 import { TGame, TPlayer, TPlayers, TPreferences, TTimer } from '../utils/types';
 
+// Action type for reducer.
 export type TActionMap<M extends { [index: string]: unknown }> = {
   [Key in keyof M]: M[Key] extends undefined
     ? {
