0633dca9d936d1d9881ee273da72a91ef02fa068
sulhadin <sulhadin@gmail.com>
2021-02-10 10:01:07 +0300

    docs(randomize.ts): Added code documentation.

--- src/utils/randomize.ts
+++ src/utils/randomize.ts
@@ -1,7 +1,9 @@
 /**
  * Always returns a random number between min (included) and max (included)
- * @param start
- * @param end
+ *
+ * @param {number} start - A number that determines the beginning point
+ * @param {number} end - A number that determines the end point
+ * @return {number} - Returns a random number between start-end range.
  */
 const randomize = (start: number, end: number): number => {
   return Math.floor(Math.random() * (end - start + 1)) + start;
