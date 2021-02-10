f138b4c7c2a8b9624ed43b095f7d99eca962eef4
sulhadin <sulhadin@gmail.com>
2021-02-10 23:06:44 +0300

    refactor(useAIPlayers.ts): Changed nullable word to string.
    * Documentation typo

--- src/hooks/useAIPlayers.ts
+++ src/hooks/useAIPlayers.ts
@@ -4,15 +4,15 @@ import textToSpeech from '../utils/textToSpeech';
 import { TAIPlayers } from './types';
 
 /**
- * Gets response from ai and vocalize it and returns the response along with play function.
+ * Gets response from ai and utters it and returns the response along with play function.
  *
- * Response result should be vocalized since response can be found or not.
+ * Response result should be utters since response can be found or not.
  *
  * @param {string} playerType - Current player type Computer | HUMAN | ...
- * @param {string=} word - Last word
+ * @param {string} word - Last word
  * @return {TAIPlayers} Returns ai response.
  */
-const useAIPlayers = (playerType: string, word?: string): TAIPlayers => {
+const useAIPlayers = (playerType: string, word: string): TAIPlayers => {
   const { response, play } = useAIPlay(playerType, word);
 
   // textToSpeech memoized one time and supplied with 'tr' parameter as default
