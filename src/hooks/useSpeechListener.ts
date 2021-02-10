799e73b182fc652eec8a144df13d9cbb55255235
sulhadin <sulhadin@gmail.com>
2021-02-10 17:20:43 +0300

    docs(useSpeechListener.ts): Adds code documentation.

--- src/hooks/useSpeechListener.ts
+++ src/hooks/useSpeechListener.ts
@@ -2,6 +2,14 @@ import { useEffect, useState } from 'react';
 
 type TSpeechListener = [string | undefined, boolean, () => void, () => void, () => void];
 
+/**
+ * A Reach hook that listens and get gets results from speech events; 'speechResultEvent', 'speechStopEvent'.
+ *
+ * @func useSpeechListener
+ * @memberOf React
+ * @param {boolean} speechStarted - Initially listening should start or stop.
+ * @return {TSpeechListener} - Returns speech result along with speech information.
+ */
 const useSpeechListener = (speechStarted: boolean): TSpeechListener => {
   const [speech, setSpeech] = useState<string>();
   const [speechStopped, setSpeechStopped] = useState<boolean>(!speechStarted);
